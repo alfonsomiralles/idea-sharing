f027115760cd7edc8b9110c98aad6a56af2a7e53
alfonsomiralles <alfonsomiralles1@gmail.com>
2023-10-02 23:29:47 +0200

    feat:user get all his ideas ordered by date

--- users/graphql/queries.py
+++ users/graphql/queries.py
@@ -4,10 +4,17 @@ from .types import IdeaType
 from users.models import Idea
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
+    """
+    Query object types for GraphQL API
+    """
     my_ideas = graphene.List(IdeaType)
 
-    def resolve_my_ideas(self, info):
+    def resolve_my_ideas(self, info) -> 'Query':
+        """
+        Fetches the logged-in user's ideas, ordered by creation date (newest to oldest).
+        """
         user = info.context.user
         if user.is_anonymous:
             raise Exception('Not logged in!')
+        
         return Idea.objects.filter(user=user).order_by('-created_at')
