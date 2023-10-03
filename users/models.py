6f6f725404b022569212910024ee581656ab2bf5
alfonsomiralles <alfonsomiralles1@gmail.com>
2023-10-03 21:57:06 +0200

    feat:user can send follow request
    user can see all follow requests
    
    user can approve or deny follow requests

--- users/models.py
+++ users/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(blank=False, max_length=255, verbose_name="email")
     following = models.ManyToManyField("self", symmetrical=False, related_name="followers")
+    follow_requests = models.ManyToManyField(
+        "self", symmetrical=False, related_name="follow_requests_received", blank=True)
 
     USERNAME_FIELD = "username"
     EMAIL_FIELD = "email"
--- users/schema.py
+++ users/schema.py
@@ -9,36 +9,109 @@ class UserType(DjangoObjectType):
         model = User
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
-    pass
 
-class FollowUser(graphene.Mutation):
+    follow_requests = graphene.List(UserType)
+    
+    def resolve_follow_requests(self, info):
+        """
+        Retrieve follow requests received by the logged-in user.
+        """
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not logged in!')
+
+        return user.follow_requests_received.all()
+class SendFollowRequest(graphene.Mutation):
     """
-    Allows a user to follow another user.
+    Send a follow request to another user.
     """
     class Arguments:
         username_to_follow = graphene.String(required=True)
-    
+
     success = graphene.Boolean()
-    follower = graphene.Field(UserType)
-    following = graphene.Field(UserType)
-    
-    def mutate(self, info, username_to_follow: str) -> 'FollowUser':
+
+    def mutate(self, info, username_to_follow: str) -> 'SendFollowRequest':
         """
-        Enable a user to follow another user by their username.
+        Send a follow request.
         """
         user = info.context.user
         if user.is_anonymous:
             raise Exception('Not logged in!')
         
         try:
-            to_follow = User.objects.get(username=username_to_follow)
+            target_user = User.objects.get(username=username_to_follow)
         except User.DoesNotExist:
             raise Exception('User to follow does not exist!')
+
+        # Check if the user is already following the target user
+        if target_user in user.following.all():
+            raise Exception('You are already following this user!')
+
+        # Check if a follow request has already been sent
+        if target_user in user.follow_requests_received.all():
+            raise Exception('Follow request already sent!')
+
+        target_user.follow_requests_received.add(user)
+        return SendFollowRequest(success=True)
+
+class ApproveFollowRequest(graphene.Mutation):
+    """
+    Approve a received follow request.
+    """
+    class Arguments:
+        username_to_approve = graphene.String(required=True)
+
+    success = graphene.Boolean()
+
+    def mutate(self, info, username_to_approve: str) -> 'ApproveFollowRequest':
+        """
+        Approve a follow request.
+        """
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not logged in!')
         
-        user.following.add(to_follow)
-        user.save()
+        try:
+            requester = User.objects.get(username=username_to_approve)
+        except User.DoesNotExist:
+            raise Exception('User does not exist!')
+
+        if requester not in user.follow_requests_received.all():
+            raise Exception('No follow request from this user!')
         
-        return FollowUser(success=True, follower=user, following=to_follow)
+        user.follow_requests_received.remove(requester)
+        requester.following.add(user)
+        return ApproveFollowRequest(success=True)
+    
+class DenyFollowRequest(graphene.Mutation):
+    """
+    Deny a received follow request.
+    """
+    class Arguments:
+        username_to_deny = graphene.String(required=True)
+
+    success = graphene.Boolean()
+
+    def mutate(self, info, username_to_deny: str) -> 'DenyFollowRequest':
+        """
+        Deny a follow request.
+        """
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not logged in!')
+        
+        try:
+            requester = User.objects.get(username=username_to_deny)
+        except User.DoesNotExist:
+            raise Exception('User does not exist!')
+
+        if requester not in user.follow_requests_received.all():
+            raise Exception('No follow request from this user!')
+
+        # Simply remove the requester from the list of received follow requests
+        user.follow_requests_received.remove(requester)
+        
+        return DenyFollowRequest(success=True)    
     
 class AuthMutation(graphene.ObjectType):
     """
@@ -49,7 +122,9 @@ class AuthMutation(graphene.ObjectType):
     password_change = mutations.PasswordChange.Field()
     send_password_reset_email = mutations.SendPasswordResetEmail.Field()
     password_reset = mutations.PasswordReset.Field()
-    follow_user = FollowUser.Field()
+    send_follow_request = SendFollowRequest.Field()
+    approve_follow_request = ApproveFollowRequest.Field()
+    deny_follow_request = DenyFollowRequest.Field()
 
     # django-graphql-jwt inheritances
     token_auth = mutations.ObtainJSONWebToken.Field()
